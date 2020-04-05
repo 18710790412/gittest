8c9ecacaf4e071deb180de7b23a69af60f31f8f6
18710790412 <18710790412@163.com>
2020-04-05 17:09:54 +0800

    resolve conflict

--- demo/index.js
+++ demo/index.js
@@ -6,6 +6,8 @@ var test4 = 'this is a test one';
 var test5 = '中国人民很行';
 var test6 = '中国人民很行哈哈哈';
 var test7 = '侯小轩来了';
+var test8 = '侯小轩用来测试的';
+
 var test8 = 'adfefadfefads';
 var test9 = '侯小轩来了';
 
